b2cc2f25f3d69ff4db754a6da2d33d58f7a14ce6
Mehdi.Zare <mehdi.zare@nav.no>
2024-10-15 15:24:29 +0200

    Lagt inn props til kafka

--- src/main/kotlin/no/nav/helsearbeidsgiver/kafka/KafkaConsumerConfig.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/kafka/KafkaConsumerConfig.kt
@@ -9,8 +9,8 @@ import org.apache.kafka.common.serialization.StringDeserializer
 import java.util.Properties
 
 fun createKafkaConsumerConfig(): Properties {
-    val PKCS12 = "PKCS12"
-    val JAVA_KEYSTORE = "jks"
+    val pkcs12 = "PKCS12"
+    val javaKeyStore = "jks"
     return Properties().apply {
         put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, Env.getPropertyOrNull("KAFKA_BROKERS") ?: "localhost:9092")
         put(ConsumerConfig.GROUP_ID_CONFIG, Env.getPropertyOrNull("KAFKA_GROUP_ID") ?: "helsearbeidsgiver-sykepenger-im-lps-api-v1")
@@ -23,13 +23,12 @@ fun createKafkaConsumerConfig(): Properties {
         put(ConsumerConfig.CLIENT_ID_CONFIG, "sykepenger-im-lps-api")
         put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, SecurityProtocol.SSL.name)
         put(SslConfigs.SSL_ENDPOINT_IDENTIFICATION_ALGORITHM_CONFIG, "")
-        put(SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG, JAVA_KEYSTORE)
-        put(SslConfigs.SSL_KEYSTORE_TYPE_CONFIG, PKCS12)
+        put(SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG, javaKeyStore)
+        put(SslConfigs.SSL_KEYSTORE_TYPE_CONFIG, pkcs12)
         put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, Env.getPropertyOrNull("KAFKA_TRUSTSTORE_PATH"))
-        put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG,  Env.getPropertyOrNull("KAFKA_CREDSTORE_PASSWORD"))
-        put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG,  Env.getPropertyOrNull("KAFKA_KEYSTORE_PATH"))
-        put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG,  Env.getPropertyOrNull("KAFKA_CREDSTORE_PASSWORD"))
-        put(SslConfigs.SSL_KEY_PASSWORD_CONFIG,  Env.getPropertyOrNull("KAFKA_CREDSTORE_PASSWORD"))
-
+        put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, Env.getPropertyOrNull("KAFKA_CREDSTORE_PASSWORD"))
+        put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, Env.getPropertyOrNull("KAFKA_KEYSTORE_PATH"))
+        put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, Env.getPropertyOrNull("KAFKA_CREDSTORE_PASSWORD"))
+        put(SslConfigs.SSL_KEY_PASSWORD_CONFIG, Env.getPropertyOrNull("KAFKA_CREDSTORE_PASSWORD"))
     }
 }
