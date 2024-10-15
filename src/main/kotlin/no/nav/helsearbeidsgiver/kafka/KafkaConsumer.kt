dc4217edaed006d72e38f8efe7c76cbe55efcec8
Mehdi.Zare <mehdi.zare@nav.no>
2024-10-15 15:35:07 +0200

    Kafka consumer

--- src/main/kotlin/no/nav/helsearbeidsgiver/kafka/KafkaConsumer.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/kafka/KafkaConsumer.kt
@@ -1,31 +1,30 @@
 package no.nav.helsearbeidsgiver.kafka
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.kafka.clients.consumer.KafkaConsumer
 import java.time.Duration
 
-fun startKafkaConsumer(
+suspend fun startKafkaConsumer(
     topic: String,
     lpsKafkaConsumer: LpsKafkaConsumer,
 ) {
     val consumer = KafkaConsumer<String, String>(createKafkaConsumerConfig() as Map<String, Any>)
     consumer.subscribe(listOf(topic))
-    var running = true
-    while (running) {
-        val records = consumer.poll(Duration.ofMillis(10))
-        for (record in records) {
-            lpsKafkaConsumer.handleRecord(record)
-            consumer.commitSync()
-        }
+
+    consumer.asFlow().collect { record ->
+        lpsKafkaConsumer.handleRecord(record)
+        consumer.commitSync()
     }
 }
 
-// fun <K, V> KafkaConsumer<K, V>.asFlow(timeout: Duration = Duration.ofMillis(10)): Flow<ConsumerRecord<K, V>> =
-//    flow {
-//        while (true) {
-//            poll(timeout).forEach { emit(it) }
-//        }
-//    }
+fun <K, V> KafkaConsumer<K, V>.asFlow(timeout: Duration = Duration.ofMillis(10)): Flow<ConsumerRecord<K, V>> =
+    flow {
+        while (true) {
+            poll(timeout).forEach { emit(it) }
+        }
+    }
 
 interface LpsKafkaConsumer {
     fun handleRecord(record: ConsumerRecord<String, String>)
