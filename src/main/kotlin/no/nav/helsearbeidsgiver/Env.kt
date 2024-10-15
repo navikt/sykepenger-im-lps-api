289dd36e2934e11c6f20d67a673841206171b042
Morten Byhring <morten.byhring@nav.no>
2024-10-15 10:40:19 +0200

    lint

--- src/main/kotlin/no/nav/helsearbeidsgiver/Env.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/Env.kt
@@ -6,11 +6,7 @@ import io.ktor.server.config.HoconApplicationConfig
 private val appConfig = HoconApplicationConfig(ConfigFactory.load())
 
 object Env {
-    fun getProperty(prop: String): String {
-        return System.getProperty(prop) ?: appConfig.property(prop).getString()
-    }
+    fun getProperty(prop: String): String = System.getProperty(prop) ?: appConfig.property(prop).getString()
 
-    fun getPropertyOrNull(prop: String): String? {
-        return System.getProperty(prop) ?: appConfig.propertyOrNull(prop)?.getString()
-    }
+    fun getPropertyOrNull(prop: String): String? = System.getProperty(prop) ?: appConfig.propertyOrNull(prop)?.getString()
 }
