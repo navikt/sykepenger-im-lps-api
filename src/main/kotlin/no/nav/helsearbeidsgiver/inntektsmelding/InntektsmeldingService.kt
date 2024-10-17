720072d648364ba60a330cf6735031dd59d5dc99
Mehdi.Zare <mehdi.zare@nav.no>
2024-10-17 14:48:47 +0200

    Lagt inn repo for mottak

--- src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/InntektsmeldingService.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/InntektsmeldingService.kt
@@ -4,9 +4,5 @@ import no.nav.helsearbeidsgiver.domene.inntektsmelding.v1.Inntektsmelding
 import no.nav.helsearbeidsgiver.utils.wrapper.Orgnr
 
 class InntektsmeldingService {
-    fun hentInntektsmeldingerByOrgNr(orgnr: Orgnr): List<Inntektsmelding> {
-
-        return emptyList()
-    }
-
+    fun hentInntektsmeldingerByOrgNr(orgnr: Orgnr): List<Inntektsmelding> = emptyList()
 }
