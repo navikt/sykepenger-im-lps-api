5e39d0ae961d0a515c035455bac48d67bdac254d
Morten Byhring <morten.byhring@nav.no>
2024-10-23 14:55:45 +0200

    quick-fix for nytt payloadformat på FORESPOERSEL_MOTTATT fra simba

--- src/main/kotlin/no/nav/helsearbeidsgiver/kafka/inntecktsmelding/InntektsmeldingKafkaConsumer.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/kafka/inntecktsmelding/InntektsmeldingKafkaConsumer.kt
@@ -4,6 +4,7 @@ import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonElement
 import no.nav.helsearbeidsgiver.db.Database.getForespoerselRepo
 import no.nav.helsearbeidsgiver.domene.inntektsmelding.v1.Inntektsmelding
 import no.nav.helsearbeidsgiver.forespoersel.Forespoersel
@@ -11,8 +12,11 @@ import no.nav.helsearbeidsgiver.inntektsmelding.ExposedMottak
 import no.nav.helsearbeidsgiver.inntektsmelding.ImMottakRepository
 import no.nav.helsearbeidsgiver.inntektsmelding.InntektsMeldingRepository
 import no.nav.helsearbeidsgiver.kafka.LpsKafkaConsumer
+import no.nav.helsearbeidsgiver.utils.json.fromJson
 import no.nav.helsearbeidsgiver.utils.json.jsonConfig
 import no.nav.helsearbeidsgiver.utils.json.serializer.UuidSerializer
+import no.nav.helsearbeidsgiver.utils.wrapper.Fnr
+import no.nav.helsearbeidsgiver.utils.wrapper.Orgnr
 import org.slf4j.LoggerFactory
 import java.util.UUID
 
@@ -26,28 +30,54 @@ class InntektsmeldingKafkaConsumer : LpsKafkaConsumer {
 
     override fun handleRecord(record: String) {
         // TODO: gjør dette i en transaksjon og gjør det skikkelig..
-        logger.info("Received record: $record")
         try {
             val obj = jsonMapper.decodeFromString<RapidMessage>(record)
-            println(obj.eventname)
+
+            logger.info("Received event: ${obj.eventname}")
             runBlocking {
                 ImMottakRepository().opprett(ExposedMottak(record))
             }
 
             if (obj.eventname == "INNTEKTSMELDING_DISTRIBUERT") {
-                runBlocking {
-                    InntektsMeldingRepository().opprett(
-                        im = jsonMapper.encodeToString(Inntektsmelding.serializer(), obj.inntektsmelding!!),
-                        org = obj.inntektsmelding.avsender.orgnr.verdi,
-                        sykmeldtFnr = obj.inntektsmelding.sykmeldt.fnr.verdi,
-                    )
+                if (obj.inntektsmelding != null) {
+                    runBlocking {
+                        InntektsMeldingRepository().opprett(
+                            im = jsonMapper.encodeToString(Inntektsmelding.serializer(), obj.inntektsmelding),
+                            org = obj.inntektsmelding.avsender.orgnr.verdi,
+                            sykmeldtFnr = obj.inntektsmelding.sykmeldt.fnr.verdi,
+                        )
+                    }
+                } else {
+                    logger.warn("Ugyldig event - mangler felt inntektsmelding, kan ikke lagre")
                 }
             } else if (obj.eventname == "FORESPOERSEL_MOTTATT") {
-                getForespoerselRepo().lagreForespoersel(
-                    forespoerselId = obj.forespoerselId!!.toString(),
-                    organisasjonsnummer = obj.orgnr!!.toString(),
-                    foedselsnr = obj.fnr!!.toString(),
-                )
+                if (obj.behov == null) {
+                    val forespoerselId =
+                        obj.data
+                            ?.get("forespoerselId")
+                            ?.fromJson(UuidSerializer)
+                            ?.toString()
+                    val orgnr =
+                        obj.data
+                            ?.get("orgnrUnderenhet")
+                            ?.fromJson(Orgnr.serializer())
+                            ?.verdi
+                    val fnr =
+                        obj.data
+                            ?.get("fnr")
+                            ?.fromJson(Fnr.serializer())
+                            ?.verdi
+
+                    if (forespoerselId != null && orgnr != null && fnr != null) {
+                        getForespoerselRepo().lagreForespoersel(
+                            forespoerselId = forespoerselId.toString(),
+                            organisasjonsnummer = orgnr.toString(),
+                            foedselsnr = fnr,
+                        )
+                    } else {
+                        logger.warn("Ugyldige verdier, kan ikke lagre!")
+                    }
+                }
             } else if (obj.eventname == "FORESPOERSEL_BESVART") {
                 settBesvart(obj.forespoerselId.toString())
             } else if (obj.eventname == "FORESPOERSEL_FORKASTET") {
@@ -79,10 +109,10 @@ class InntektsmeldingKafkaConsumer : LpsKafkaConsumer {
     @Serializable
     data class RapidMessage(
         @SerialName("@event_name") val eventname: String,
+        @SerialName("@behov") val behov: String? = null,
+        val data: Map<String, JsonElement>? = emptyMap(),
         val inntektsmelding: Inntektsmelding? = null,
         val forespoersel: Forespoersel? = null,
         @Serializable(with = UuidSerializer::class) val forespoerselId: UUID? = null,
-        @SerialName("identitetsnummer") val fnr: String? = null,
-        @SerialName("orgnrUnderenhet") val orgnr: String? = null,
     )
 }
--- src/test/kotlin/no/nav/helsearbeidsgiver/kafka/inntecktsmelding/InntektsmeldingKafkaConsumerTest.kt
+++ src/test/kotlin/no/nav/helsearbeidsgiver/kafka/inntecktsmelding/InntektsmeldingKafkaConsumerTest.kt
@@ -1,15 +1,28 @@
 package no.nav.helsearbeidsgiver.kafka.inntecktsmelding
 
 import no.nav.helsearbeidsgiver.db.Database
-import org.junit.Ignore
+import org.junit.jupiter.api.Test
 
 class InntektsmeldingKafkaConsumerTest {
-    @Ignore
+    @Test
     fun handleRecord() {
         Database.init()
         val payload = """
-            {"@event_name":"INNTEKTSMELDING_DISTRIBUERT","uuid":"e842491c-d717-4ca3-b3f1-b81ef75c4739","journalpostId":"671159009","inntektsmelding":{"id":"69297657-fdbe-451b-b77e-43ae3aa90e29","type":{"type":"Forespurt","id":"724fb233-2175-4b58-8f47-181f24547f30"},"sykmeldt":{"fnr":"22518249472","navn":"OVERFØLSOM GRAVEMASKIN"},"avsender":{"orgnr":"810007842","orgNavn":"ANSTENDIG PIGGSVIN BARNEHAGE","navn":"BERØMT FLYTTELASS","tlf":"12345678"},"sykmeldingsperioder":[{"fom":"2024-06-01","tom":"2024-06-30"}],"agp":{"perioder":[{"fom":"2024-06-01","tom":"2024-06-16"}],"egenmeldinger":[],"redusertLoennIAgp":{"beloep":1000.0,"begrunnelse":"BetvilerArbeidsufoerhet"}},"inntekt":{"beloep":60000.0,"inntektsdato":"2024-06-01","naturalytelser":[],"endringAarsak":null},"refusjon":null,"aarsakInnsending":"Endring","mottatt":"2024-10-18T13:18:28.592508012+02:00","vedtaksperiodeId":"7e7c6173-ea6a-4228-8b18-d1c3e3e0af7c"},"bestemmende_fravaersdag":"2024-06-01","@id":"88a3b4e0-bac8-45e4-a128-7cd89cdb515d","@opprettet":"2024-10-18T13:18:29.434466991","system_read_count":0,"system_participating_services":[{"id":"88a3b4e0-bac8-45e4-a128-7cd89cdb515d","time":"2024-10-18T13:18:29.434466991","service":"im-distribusjon","instance":"im-distribusjon-56d6fc7859-rh4g5","image":"ghcr.io/navikt/helsearbeidsgiver-inntektsmelding/im-distribusjon:a180e3d"}],"@forårsaket_av":{"id":"ccbb8e0e-9560-4a52-bd84-056f7e7b348f","opprettet":"2024-10-18T13:18:29.371630591","event_name":"INNTEKTSMELDING_JOURNALFOERT"}}
+            {"@event_name":"FORESPOERSEL_MOTTATT","uuid":"9c87f626-fe80-4618-ad37-f7e6a7026bc7","data":{"forespoerselId":"556d6430-0c43-4dbc-8040-36ba37bfa191","orgnrUnderenhet":"810007842","fnr":"22518249472","skal_ha_paaminnelse":true},"@id":"44df174f-3496-485d-ae3a-517b532aee70","@opprettet":"2024-10-23T12:52:56.081140841","system_read_count":0,"system_participating_services":[{"id":"44df174f-3496-485d-ae3a-517b532aee70","time":"2024-10-23T12:52:56.081140841","service":"im-forespoersel-mottatt","instance":"im-forespoersel-mottatt-58f979bbff-rbdst","image":"ghcr.io/navikt/helsearbeidsgiver-inntektsmelding/im-forespoersel-mottatt:d79b643"}],"@forårsaket_av":{"id":"67b446f5-167f-4167-a590-91df7fccd66b","opprettet":"2024-10-23T12:52:56.077268111"}}
         """
+
+        val payload2 =
+            """
+            {"@event_name":"FORESPOERSEL_BESVART","uuid":"b52d4703-48c9-4ada-bcba-a088f1acab96","forespoerselId":"556d6430-0c43-4dbc-8040-36ba37bfa191","@id":"ce1289a0-1554-4b41-8307-ed2396b59846","@opprettet":"2024-10-23T12:54:03.432888987","system_read_count":0,"system_participating_services":[{"id":"ce1289a0-1554-4b41-8307-ed2396b59846","time":"2024-10-23T12:54:03.432888987","service":"im-forespoersel-besvart","instance":"im-forespoersel-besvart-788d6bdbd-qqrw9","image":"ghcr.io/navikt/helsearbeidsgiver-inntektsmelding/im-forespoersel-besvart:d79b643"}],"@forårsaket_av":{"id":"b584f32a-ca76-481f-8cf1-37c31d51b6f7","opprettet":"2024-10-23T12:54:03.413234137","event_name":"INNTEKTSMELDING_MOTTATT"}}
+            """.trimIndent()
+
+        val payload3 =
+            """
+            {"@event_name":"INNTEKTSMELDING_DISTRIBUERT","uuid":"c13943eb-e4be-47e9-9ae4-8fd9f09abf2e","journalpostId":"671159571","inntektsmelding":{"id":"d09b2674-9f2c-439f-9a60-9fe84ca117b5","type":{"type":"Forespurt","id":"8e43298c-8083-4daf-8092-8500e6524bbd"},"sykmeldt":{"fnr":"05499120674","navn":"FANTASTISK FERSKVANN"},"avsender":{"orgnr":"810007842","orgNavn":"ANSTENDIG PIGGSVIN BARNEHAGE","navn":"BERØMT FLYTTELASS","tlf":"99999999"},"sykmeldingsperioder":[{"fom":"2024-07-01","tom":"2024-07-31"}],"agp":{"perioder":[{"fom":"2024-07-01","tom":"2024-07-16"}],"egenmeldinger":[],"redusertLoennIAgp":null},"inntekt":{"beloep":20000.0,"inntektsdato":"2024-07-01","naturalytelser":[],"endringAarsak":null},"refusjon":null,"aarsakInnsending":"Ny","mottatt":"2024-10-23T14:32:16.708400904+02:00","vedtaksperiodeId":"c6edb839-2fb5-4602-8f88-c70693f33015"},"bestemmende_fravaersdag":"2024-07-01","@id":"6d189fc1-79f7-4ecd-9d28-671a264df06f","@opprettet":"2024-10-23T14:32:17.596232993","system_read_count":0,"system_participating_services":[{"id":"6d189fc1-79f7-4ecd-9d28-671a264df06f","time":"2024-10-23T14:32:17.596232993","service":"im-distribusjon","instance":"im-distribusjon-798b4856fc-82qt6","image":"ghcr.io/navikt/helsearbeidsgiver-inntektsmelding/im-distribusjon:3285d5d"}],"@forårsaket_av":{"id":"48715ec3-e08c-417e-bbce-78327e136482","opprettet":"2024-10-23T14:32:17.368784914","event_name":"INNTEKTSMELDING_JOURNALFOERT"}}
+            """.trimIndent()
         InntektsmeldingKafkaConsumer().handleRecord(payload)
+
+        InntektsmeldingKafkaConsumer().handleRecord(payload2)
+        InntektsmeldingKafkaConsumer().handleRecord(payload3)
     }
 }
