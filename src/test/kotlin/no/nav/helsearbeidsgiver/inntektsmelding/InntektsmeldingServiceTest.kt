7b95bf2bbd48d2613c353c31d5f56989489c6a12
Mehdi.Zare <mehdi.zare@nav.no>
2024-11-05 10:19:26 +0100

    InntektsmeldingService tester

--- src/test/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/InntektsmeldingServiceTest.kt
+++ src/test/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/InntektsmeldingServiceTest.kt
@@ -1,10 +1,13 @@
 package no.nav.helsearbeidsgiver.inntektsmelding
 
+import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
 import no.nav.helsearbeidsgiver.domene.inntektsmelding.v1.Inntektsmelding
 import no.nav.helsearbeidsgiver.utils.buildInntektsmeldingJson
 import org.junit.jupiter.api.Test
+import java.time.LocalDateTime
+import kotlin.test.assertEquals
 
 class InntektsmeldingServiceTest {
     val inntektsmeldingRepository = mockk<InntektsmeldingRepository>()
@@ -29,5 +32,63 @@ class InntektsmeldingServiceTest {
                 forespoerselID = inntektsmelding.type.id.toString(),
             )
         }
+    }
+
+    @Test
+    fun `hentInntektsmeldingerByOrgNr should call inntektsmeldingRepository`() {
+        val orgnr = "123456789"
+        inntektsmeldingService.hentInntektsmeldingerByOrgNr(orgnr)
+
+        verify {
+            inntektsmeldingRepository.hent(orgnr)
+        }
+    }
+
+    @Test
+    fun `hentInntektsMeldingByRequest should call inntektsmeldingRepository`() {
+        val foresporselid = "123456789"
+        val orgnr = "987654322"
+        val fnr = "12345678901"
+        val datoFra = LocalDateTime.now()
+        val datoTil = datoFra.plusDays(1)
+        val request =
+            InntektsmeldingRequest(
+                fnr = fnr,
+                foresporselid = foresporselid,
+                datoFra = datoFra,
+                datoTil = datoTil,
+            )
+        inntektsmeldingService.hentInntektsMeldingByRequest(orgnr, request)
+
+        verify {
+            inntektsmeldingRepository.hent(orgNr = orgnr, request = request)
+        }
+    }
+
+    @Test
+    fun `hentInntektsmeldingerByOrgNr should return empty list on failure`() {
+        val orgnr = "123456789"
+        every { inntektsmeldingRepository.hent(orgnr) } throws Exception()
+        val hentInntektsmeldingerByOrgNr = inntektsmeldingService.hentInntektsmeldingerByOrgNr(orgnr)
+        assertEquals(emptyList(), hentInntektsmeldingerByOrgNr)
+    }
+
+    @Test
+    fun `hentInntektsMeldingByRequest should return empty list on failure`() {
+        val orgnr = "987654322"
+        val fnr = "12345678901"
+        val foresporselid = "123456789"
+        val datoFra = LocalDateTime.now()
+        val datoTil = datoFra.plusDays(1)
+        val request =
+            InntektsmeldingRequest(
+                fnr = fnr,
+                foresporselid = foresporselid,
+                datoFra = datoFra,
+                datoTil = datoTil,
+            )
+        every { inntektsmeldingRepository.hent(orgNr = orgnr, request = request) } throws Exception()
+        val hentInntektsMeldingByRequest = inntektsmeldingService.hentInntektsMeldingByRequest(orgnr, request)
+        assertEquals(InntektsmeldingResponse(0, emptyList()), hentInntektsMeldingByRequest)
     }
 }
