bd489527d0df9d903137a811354cdc10be655d3e
Morten Byhring <morten.byhring@nav.no>
2024-11-20 15:14:48 +0100

    cleanup testdata

--- src/test/kotlin/no/nav/helsearbeidsgiver/utils/TestData.kt
+++ src/test/kotlin/no/nav/helsearbeidsgiver/utils/TestData.kt
@@ -61,20 +61,20 @@ object TestData {
         }
         """
 
-    val payload3 =
+    const val IM_MOTTATT =
         """
         {"journalpostId":"671166144","inntektsmeldingV1":{"id":"01f37507-24fa-4266-9f70-42b97f9eb527","type":{"type":"Forespurt","id":"034f161b-d43b-4e06-9563-3fe5fbd15442"},"sykmeldt":{"fnr":"01447842099","navn":"KONKURRANSEDYKTIG HANDLINGSROM"},"avsender":{"orgnr":"810007982","orgNavn":"ANSTENDIG PIGGSVIN SYKEHJEM","navn":"BERØMT FLYTTELASS","tlf":"11223344"},"sykmeldingsperioder":[{"fom":"2024-08-01","tom":"2024-08-31"}],"agp":{"perioder":[{"fom":"2024-08-01","tom":"2024-08-16"}],"egenmeldinger":[],"redusertLoennIAgp":null},"inntekt":{"beloep":24667.33,"inntektsdato":"2024-08-01","naturalytelser":[],"endringAarsak":null},"refusjon":null,"aarsakInnsending":"Ny","mottatt":"2024-11-07T15:12:52.207740699+01:00","vedtaksperiodeId":"d90503a1-c15a-41ca-9cb8-cbc729d63893"},"bestemmendeFravaersdag":"2024-08-01","inntektsmelding":{"orgnrUnderenhet":"810007982","identitetsnummer":"01447842099","fulltNavn":"KONKURRANSEDYKTIG HANDLINGSROM","virksomhetNavn":"ANSTENDIG PIGGSVIN SYKEHJEM","behandlingsdager":[],"egenmeldingsperioder":[],"fraværsperioder":[{"fom":"2024-08-01","tom":"2024-08-31"}],"arbeidsgiverperioder":[{"fom":"2024-08-01","tom":"2024-08-16"}],"beregnetInntekt":24667.33,"inntektsdato":"2024-08-01","inntekt":{"bekreftet":true,"beregnetInntekt":24667.33,"manueltKorrigert":false},"fullLønnIArbeidsgiverPerioden":{"utbetalerFullLønn":true},"refusjon":{"utbetalerHeleEllerDeler":false},"naturalytelser":[],"tidspunkt":"2024-11-07T15:12:52.207740699+01:00","årsakInnsending":"NY","innsenderNavn":"BERØMT FLYTTELASS","telefonnummer":"11223344","forespurtData":["arbeidsgiverperiode","inntekt"],"bestemmendeFraværsdag":"2024-08-01","vedtaksperiodeId":"d90503a1-c15a-41ca-9cb8-cbc729d63893"},"selvbestemt":false}
-        """.trimIndent()
+        """
 
-    val payload4 =
+    const val ARBEIDSGIVER_INITIERT_IM_MOTTATT =
         """
         {"journalpostId":"671166582","inntektsmeldingV1":{"id":"2f7685d0-fe65-48c6-96b9-7b5189358ab1","type":{"type":"Selvbestemt","id":"24428a05-6826-4a01-a6be-30fb15816a6e"},"sykmeldt":{"fnr":"10107400090","navn":"BERØMT FLYTTELASS"},"avsender":{"orgnr":"810007842","orgNavn":"ANSTENDIG PIGGSVIN BARNEHAGE","navn":"BERØMT FLYTTELASS","tlf":"12345678"},"sykmeldingsperioder":[{"fom":"2024-08-01","tom":"2024-08-08"}],"agp":{"perioder":[{"fom":"2024-08-01","tom":"2024-08-08"}],"egenmeldinger":[],"redusertLoennIAgp":{"beloep":23456.0,"begrunnelse":"ArbeidOpphoert"}},"inntekt":{"beloep":54000.0,"inntektsdato":"2024-08-01","naturalytelser":[],"endringAarsak":null},"refusjon":null,"aarsakInnsending":"Ny","mottatt":"2024-11-12T14:04:07.557238646+01:00","vedtaksperiodeId":"dc2b6464-d606-4c2d-8bb1-c5ce8d811077"},"bestemmendeFravaersdag":null,"inntektsmelding":{"orgnrUnderenhet":"810007842","identitetsnummer":"10107400090","fulltNavn":"BERØMT FLYTTELASS","virksomhetNavn":"ANSTENDIG PIGGSVIN BARNEHAGE","behandlingsdager":[],"egenmeldingsperioder":[],"fraværsperioder":[{"fom":"2024-08-01","tom":"2024-08-08"}],"arbeidsgiverperioder":[{"fom":"2024-08-01","tom":"2024-08-08"}],"beregnetInntekt":54000.0,"inntektsdato":"2024-08-01","inntekt":{"bekreftet":true,"beregnetInntekt":54000.0,"manueltKorrigert":false},"fullLønnIArbeidsgiverPerioden":{"utbetalerFullLønn":false,"begrunnelse":"ArbeidOpphoert","utbetalt":23456.0},"refusjon":{"utbetalerHeleEllerDeler":false},"naturalytelser":[],"tidspunkt":"2024-11-12T14:04:07.557238646+01:00","årsakInnsending":"NY","innsenderNavn":"BERØMT FLYTTELASS","telefonnummer":"12345678","forespurtData":["arbeidsgiverperiode","inntekt"],"bestemmendeFraværsdag":"2024-08-01","vedtaksperiodeId":"dc2b6464-d606-4c2d-8bb1-c5ce8d811077"},"selvbestemt":true}
-        """.trimIndent()
+        """
 
-    val ikkeAktuellPayload =
+    const val SIMBA_PAYLOAD =
         """
         {"@event_name":"TILGANG_FORESPOERSEL_REQUESTED","uuid":"b52d4703-48c9-4ada-bcba-a088f1acab96","forespoerselId":"556d6430-0c43-4dbc-8040-36ba37bfa191","@id":"ce1289a0-1554-4b41-8307-ed2396b59846","@opprettet":"2024-10-23T12:54:03.432888987","system_read_count":0,"system_participating_services":[{"id":"ce1289a0-1554-4b41-8307-ed2396b59846","time":"2024-10-23T12:54:03.432888987","service":"im-forespoersel-besvart","instance":"im-forespoersel-besvart-788d6bdbd-qqrw9","image":"ghcr.io/navikt/helsearbeidsgiver-inntektsmelding/im-forespoersel-besvart:d79b643"}],"@forårsaket_av":{"id":"b584f32a-ca76-481f-8cf1-37c31d51b6f7","opprettet":"2024-10-23T12:54:03.413234137","event_name":"INNTEKTSMELDING_MOTTATT"}}
-        """.trimIndent()
+        """
 
     fun forespoerselDokument(
         orgnr: String,
