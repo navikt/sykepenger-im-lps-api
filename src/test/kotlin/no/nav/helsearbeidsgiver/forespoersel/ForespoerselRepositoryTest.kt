bb3039063964e9e4fd5b850d387b9a49d9409492
Mehdi.Zare <mehdi.zare@nav.no>
2024-11-23 07:42:09 +0100

    Tester

--- src/test/kotlin/no/nav/helsearbeidsgiver/forespoersel/ForespoerselRepositoryTest.kt
+++ src/test/kotlin/no/nav/helsearbeidsgiver/forespoersel/ForespoerselRepositoryTest.kt
@@ -25,4 +25,51 @@ class ForespoerselRepositoryTest {
         forespoerselRepository.settBesvart(forespoerselID)
         assertEquals(Status.MOTTATT, forespoerselRepository.hentForespoersel(forespoerselID)?.status)
     }
+
+    @Test
+    fun lagreForespoerselDuplikat() {
+        val forespoerselID = UUID.randomUUID().toString()
+        forespoerselRepository.lagreForespoersel(forespoerselID, forespoerselDokument(DEFAULT_ORG, DEFAULT_FNR))
+
+        forespoerselRepository.lagreForespoersel(forespoerselID, forespoerselDokument(DEFAULT_ORG, DEFAULT_FNR))
+        val forespoersler = forespoerselRepository.hentForespoerslerForOrgnr(DEFAULT_ORG)
+        assertEquals(1, forespoersler.size)
+    }
+
+    @Test
+    fun settForkastet() {
+        val forespoerselID = UUID.randomUUID().toString()
+        forespoerselRepository.lagreForespoersel(forespoerselID, forespoerselDokument(DEFAULT_ORG, DEFAULT_FNR))
+
+        forespoerselRepository.settForkastet(forespoerselID)
+        assertEquals(Status.FORKASTET, forespoerselRepository.hentForespoersel(forespoerselID)?.status)
+    }
+
+    @Test
+    fun hentForespoerslerForOrgnr() {
+        val forespoerselID1 = UUID.randomUUID().toString()
+        val forespoerselID2 = UUID.randomUUID().toString()
+        forespoerselRepository.lagreForespoersel(forespoerselID1, forespoerselDokument(DEFAULT_ORG, DEFAULT_FNR))
+        forespoerselRepository.lagreForespoersel(forespoerselID2, forespoerselDokument(DEFAULT_ORG, DEFAULT_FNR))
+
+        val forespoersler = forespoerselRepository.hentForespoerslerForOrgnr(DEFAULT_ORG)
+        assertEquals(2, forespoersler.size)
+    }
+
+    @Test
+    fun filtrerForespoersler() {
+        val forespoerselID1 = UUID.randomUUID().toString()
+        val forespoerselID2 = UUID.randomUUID().toString()
+        forespoerselRepository.lagreForespoersel(forespoerselID1, forespoerselDokument(DEFAULT_ORG, DEFAULT_FNR))
+        forespoerselRepository.lagreForespoersel(forespoerselID2, forespoerselDokument(DEFAULT_ORG, DEFAULT_FNR))
+
+        val request =
+            ForespoerselRequest(
+                fnr = DEFAULT_FNR,
+                forespoerselId = null,
+                status = null,
+            )
+        val forespoersler = forespoerselRepository.filtrerForespoersler(DEFAULT_ORG, request)
+        assertEquals(2, forespoersler.size)
+    }
 }
