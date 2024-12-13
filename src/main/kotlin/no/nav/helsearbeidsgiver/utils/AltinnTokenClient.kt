6912aa85a53028309d0f6cea8d603a8dd2bf96b3
Gustav Berggren <gustav.berggren@nav.no>
2024-12-13 12:36:39 +0100

    hent ut tokenstring fra texas

--- src/main/kotlin/no/nav/helsearbeidsgiver/utils/AltinnTokenClient.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/utils/AltinnTokenClient.kt
@@ -1,6 +1,7 @@
 package no.nav.helsearbeidsgiver.utils
 
 import io.ktor.client.HttpClient
+import io.ktor.client.call.body
 import io.ktor.client.engine.apache5.Apache5
 import io.ktor.client.request.get
 import io.ktor.client.request.header
@@ -15,14 +16,17 @@ class AltinnTokenClient {
     fun getToken(): String =
         runBlocking {
             val maskinportenToken =
-                httpClient.post(System.getenv("NAIS_TOKEN_ENDPOINT")) {
-                    setBody(
-                        mapOf(
-                            "identity_provider" to "maskinporten",
-                            "target" to "altinn:authorization/authorize",
-                        ),
-                    )
-                }
+                httpClient
+                    .post(System.getenv("NAIS_TOKEN_ENDPOINT")) {
+                        header("Content-Type", "application/json")
+                        header("Accept", "application/json")
+                        setBody(
+                            mapOf(
+                                "identity_provider" to "maskinporten",
+                                "target" to "altinn:authorization/authorize",
+                            ),
+                        )
+                    }.body<Map<String, Any>>()["access_token"] as String
             httpClient
                 .get("https://platform.tt02.altinn.no/authentication/api/v1/exchange/maskinporten") {
                     header("Authorization", "Bearer $maskinportenToken")
