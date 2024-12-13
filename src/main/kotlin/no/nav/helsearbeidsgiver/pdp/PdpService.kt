0b74aae497306101d784484f9b1c4788baf9eaa7
Gustav Berggren <gustav.berggren@nav.no>
2024-12-13 15:08:32 +0100

    logg subscription key midlertidig

--- src/main/kotlin/no/nav/helsearbeidsgiver/pdp/PdpService.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/pdp/PdpService.kt
@@ -3,6 +3,7 @@ package no.nav.helsearbeidsgiver.pdp
 import kotlinx.coroutines.runBlocking
 import no.nav.helsearbeidsgiver.altinn.pdp.PdpClient
 import no.nav.helsearbeidsgiver.utils.AltinnTokenClient
+import no.nav.helsearbeidsgiver.utils.log.sikkerLogger
 
 class PdpService(
     val pdpClient: PdpClient,
@@ -22,11 +23,13 @@ class PdpService(
 }
 
 fun lagPdpClient(): PdpClient {
+    val subscriptionKey = System.getenv("SUBSCRIPTION_KEY")
+    sikkerLogger().info("subscriptionKey: $subscriptionKey")
     val altinnTokenClient = AltinnTokenClient()
     val pdpClient =
         PdpClient(
             "https://platform.tt02.altinn.no",
-            System.getenv("SUBSCRIPTION_KEY"),
+            subscriptionKey,
             "nav_sykepenger_inntektsmelding-nedlasting",
             altinnTokenClient::getToken,
         )
