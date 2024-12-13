7e5cfc7e32d05ceff32be545e267a18a1d329c4c
Gustav Berggren <gustav.berggren@nav.no>
2024-12-13 13:11:19 +0100

    Prøv med kotlinx serialisering og accept header

--- src/main/kotlin/no/nav/helsearbeidsgiver/utils/AltinnTokenClient.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/utils/AltinnTokenClient.kt
@@ -3,6 +3,7 @@ package no.nav.helsearbeidsgiver.utils
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.engine.apache5.Apache5
+import io.ktor.client.request.accept
 import io.ktor.client.request.get
 import io.ktor.client.request.header
 import io.ktor.client.request.post
@@ -12,23 +13,34 @@ import io.ktor.http.ContentType
 import io.ktor.http.contentType
 import io.ktor.http.formUrlEncode
 import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
 class AltinnTokenClient {
+    @Serializable
+    data class TokenResponse(
+        @SerialName("access_token") val accessToken: String,
+        @SerialName("token_type") val tokenType: String,
+        @SerialName("expires_in") val expiresInSeconds: Long,
+    )
+
     val httpClient = HttpClient(Apache5)
 
     fun getToken(): String =
         runBlocking {
-            val maskinportenToken =
+            val maskinportenToken: String =
                 httpClient
                     .post(System.getenv("NAIS_TOKEN_ENDPOINT")) {
                         contentType(ContentType.Application.FormUrlEncoded)
+                        accept(ContentType.Application.Json)
                         setBody(
                             listOf(
                                 "identity_provider" to "maskinporten",
                                 "target" to "altinn:authorization/authorize",
                             ).formUrlEncode(),
                         )
-                    }.body<Map<String, Any>>()["access_token"] as String
+                    }.body<TokenResponse>()
+                    .accessToken
             httpClient
                 .get("https://platform.tt02.altinn.no/authentication/api/v1/exchange/maskinporten") {
                     header("Authorization", "Bearer $maskinportenToken")
