d071edf1e6e3b73824a42ff32eaec8d587c5a001
Magnus Sælensminde <magnus.salensminde@bekk.no>
2025-02-10 13:21:55 +0100

    Fiks teit feil

--- src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/InntektsmeldingRouting.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/InntektsmeldingRouting.kt
@@ -3,29 +3,18 @@
 package no.nav.helsearbeidsgiver.inntektsmelding
 
 import io.ktor.http.HttpStatusCode
-import io.ktor.server.application.ApplicationCall
 import io.ktor.server.application.call
 import io.ktor.server.request.receive
-import io.ktor.server.request.receiveText
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
 import io.ktor.server.routing.post
-import io.ktor.util.pipeline.PipelineContext
-import kotlinx.serialization.Serializable
 import kotlinx.serialization.UseSerializers
 import no.nav.helsearbeidsgiver.auth.getConsumerOrgnr
 import no.nav.helsearbeidsgiver.auth.getSystembrukerOrgnr
 import no.nav.helsearbeidsgiver.auth.tokenValidationContext
-import no.nav.helsearbeidsgiver.domene.inntektsmelding.v1.Arbeidsgiverperiode
-import no.nav.helsearbeidsgiver.domene.inntektsmelding.v1.Inntekt
-import no.nav.helsearbeidsgiver.domene.inntektsmelding.v1.Refusjon
 import no.nav.helsearbeidsgiver.domene.inntektsmelding.v1.skjema.SkjemaInntektsmelding
-import no.nav.helsearbeidsgiver.utils.json.fromJson
-import no.nav.helsearbeidsgiver.utils.json.parseJson
 import no.nav.helsearbeidsgiver.utils.json.serializer.UuidSerializer
-import no.nav.helsearbeidsgiver.utils.json.toPretty
-import no.nav.helsearbeidsgiver.utils.log.logger
 import no.nav.helsearbeidsgiver.utils.log.sikkerLogger
 import java.util.UUID
 
@@ -78,53 +67,16 @@ fun Route.innsending(inntektsmeldingService: InntektsmeldingService) {
     // Send inn inntektsmelding
     post("/inntektsmelding") {
         try {
-            val request = lesRequestOrNull()
+            val request = this.call.receive<SkjemaInntektsmelding>()
             val sluttbrukerOrgnr = tokenValidationContext().getSystembrukerOrgnr()
             val lpsOrgnr = tokenValidationContext().getConsumerOrgnr()
             sikkerLogger().info("Mottatt innsending: $request")
             sikkerLogger().info("LPS: [$lpsOrgnr] sender inn skjema på vegne av bedrift: [$sluttbrukerOrgnr]")
-            request?.let { inntektsmeldingService.sendInn(request.toSkjemaInntektsmelding()) }
-            call.respond(HttpStatusCode.Created, UUID.randomUUID()) // Skal returnere innsendingID
+            inntektsmeldingService.sendInn(request)
+            call.respond(HttpStatusCode.Created, UUID.randomUUID().toString()) // Skal returnere innsendingID
         } catch (e: Exception) {
             sikkerLogger().error("Feil ved lagring / innsending: {$e}", e)
             call.respond(HttpStatusCode.InternalServerError, "En feil oppstod")
         }
     }
-}
-
-private suspend fun PipelineContext<Unit, ApplicationCall>.lesRequestOrNull(): ApiSkjemaInntektsmeldingV1? =
-    call
-        .receiveText()
-        .runCatching {
-            parseJson()
-                .also { json ->
-                    "Mottok inntektsmeldingsskjema.".let {
-                        logger().info(it)
-                        sikkerLogger().info("$it\n${json.toPretty()}")
-                    }
-                }.fromJson(ApiSkjemaInntektsmeldingV1.serializer())
-        }.getOrElse { error ->
-            "Klarte ikke parse json for inntektsmeldingsskjema.".also {
-                logger().error(it)
-                sikkerLogger().error(it, error)
-            }
-            null
-        }
-
-@Serializable
-data class ApiSkjemaInntektsmeldingV1(
-    val forespoerselId: UUID,
-    val avsenderTlf: String,
-    val agp: Arbeidsgiverperiode?,
-    val inntekt: Inntekt?,
-    val refusjon: Refusjon?,
-) {
-    fun toSkjemaInntektsmelding() =
-        SkjemaInntektsmelding(
-            forespoerselId = forespoerselId,
-            avsenderTlf = avsenderTlf,
-            agp = agp,
-            inntekt = inntekt,
-            refusjon = refusjon,
-        )
 }
