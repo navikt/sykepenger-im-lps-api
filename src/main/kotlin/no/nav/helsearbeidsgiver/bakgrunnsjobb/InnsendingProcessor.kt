7e6fb5250658c99b1ee0247a9112876413836efd
Mehdi.Zare <mehdi.zare@nav.no>
2025-03-11 12:53:59 +0100

    bakgrunnsjobb

--- src/main/kotlin/no/nav/helsearbeidsgiver/bakgrunnsjobb/InnsendingProcessor.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/bakgrunnsjobb/InnsendingProcessor.kt
@@ -4,8 +4,8 @@ import no.nav.hag.utils.bakgrunnsjobb.Bakgrunnsjobb
 import no.nav.hag.utils.bakgrunnsjobb.BakgrunnsjobbProsesserer
 import no.nav.helsearbeidsgiver.innsending.InnsendingRepository
 import no.nav.helsearbeidsgiver.utils.getElectedLeaderId
-import no.nav.helsearbeidsgiver.utils.getHostName
 import no.nav.helsearbeidsgiver.utils.log.sikkerLogger
+import java.util.UUID
 
 class InnsendingProcessor(
     val innsendingRepository: InnsendingRepository,
@@ -18,12 +18,12 @@ class InnsendingProcessor(
 
     override fun prosesser(jobb: Bakgrunnsjobb) {
         sikkerLogger().info(
-            "Bakgrunnsjobb: Prosesserer ${jobb.uuid} med type ${jobb.type} på ${getHostName()} elected leader: ${getElectedLeaderId()}",
+            "Bakgrunnsjobb: Prosesserer ${jobb.uuid} med type ${jobb.type} på elected leader: ${getElectedLeaderId()}",
         )
         sikkerLogger().info("Bakgrunnsjobb: Data: $jobb")
-        sikkerLogger().info(
-            @Suppress("ktlint:standard:max-line-length")
-            "Bakgrunnsjobb: jobb status: ${jobb.status} og forsøk: ${jobb.forsoek} og data: ${jobb.data} og kjoeretid: ${jobb.kjoeretid} og opprettet: ${jobb.opprettet}  og behandlet: ${jobb.behandlet}",
-        )
+        innsendingRepository.hentById(UUID.fromString(jobb.data))?.let {
+            sikkerLogger().info("Bakgrunnsjobb: Fant innsending med id: ${it.innsendingId}")
+            sikkerLogger().info("Bakgrunnsjobb: Status: ${it.status}")
+        }
     }
 }
