41ac2afb7ff2ccbfa8b9d493caf7cc7f652abffc
Magnus Sælensminde <magnus.salensminde@bekk.no>
2025-04-09 08:46:26 +0200

    Legg på litt mer logging for å se om Unleash starter opp

--- src/main/kotlin/no/nav/helsearbeidsgiver/utils/UnleashFeatureToggles.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/utils/UnleashFeatureToggles.kt
@@ -1,29 +1,33 @@
 package no.nav.helsearbeidsgiver.utils
 
 import io.getunleash.DefaultUnleash
-import io.getunleash.Unleash
 import io.getunleash.util.UnleashConfig
 import no.nav.helsearbeidsgiver.Env
+import no.nav.helsearbeidsgiver.utils.log.sikkerLogger
 
 class UnleashFeatureToggles {
     private val apiKey = Env.getProperty("UNLEASH_SERVER_API_TOKEN")
     private val apiUrl = Env.getProperty("UNLEASH_SERVER_API_URL") + "/api"
     private val apiEnv = Env.getProperty("UNLEASH_SERVER_API_ENV")
 
-    private val config: UnleashConfig =
-        UnleashConfig
-            .builder()
-            .appName("sykepenger-im-lps-api")
-            .instanceId("sykepenger-im-lps-api")
-            .unleashAPI(apiUrl)
-            .apiKey(apiKey)
-            .environment(apiEnv)
-            .build()
+    private val defaultUnleash: DefaultUnleash
 
-    private val unleash: Unleash = DefaultUnleash(config)
+    init {
+        defaultUnleash =
+            DefaultUnleash(
+                UnleashConfig
+                    .builder()
+                    .appName("sykepenger-im-lps-api")
+                    .instanceId("sykepenger-im-lps-api")
+                    .unleashAPI(apiUrl)
+                    .apiKey(apiKey)
+                    .environment(apiEnv)
+                    .build(),
+            ).also { sikkerLogger().info("Unleash koblet opp apiUrl: $apiUrl and apiEnv: $apiEnv") }
+    }
 
     fun skalOppretteDialogVedMottattSykmelding(): Boolean =
-        unleash.isEnabled(
+        defaultUnleash.isEnabled(
             "opprett-dialog-ved-mottatt-sykmelding",
             false,
         )
