780de2a2003028c1e118a59bf07be4098c8b5618
Magnus Sælensminde <magnus.salensminde@bekk.no>
2025-04-10 09:34:28 +0200

    La være å sende API-mottatte inntektsmeldinger videre til Simba i produksjon

--- src/main/kotlin/no/nav/helsearbeidsgiver/kafka/innsending/InnsendingProducer.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/kafka/innsending/InnsendingProducer.kt
@@ -4,19 +4,47 @@ import kotlinx.serialization.builtins.MapSerializer
 import kotlinx.serialization.json.JsonElement
 import no.nav.helsearbeidsgiver.Env.getProperty
 import no.nav.helsearbeidsgiver.utils.json.toJson
+import no.nav.helsearbeidsgiver.utils.json.toPretty
+import no.nav.helsearbeidsgiver.utils.log.sikkerLogger
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerRecord
 
-class InnsendingProducer(
-    private val kafkaProducer: KafkaProducer<String, JsonElement>,
-) {
+interface InnsendingProducerI {
+    fun send(vararg message: Pair<InnsendingKafka.Key, JsonElement>): Result<JsonElement>
+}
+
+class IngenInnsendingProducer : InnsendingProducerI {
     private val topic = getProperty("kafkaProducer.innsending.topic")
 
-    fun send(vararg message: Pair<InnsendingKafka.Key, JsonElement>): Result<JsonElement> =
+    override fun send(vararg message: Pair<InnsendingKafka.Key, JsonElement>): Result<JsonElement> =
+        Result
+            .success("".toJson())
+            .also { sikkerLogger().info("Publiserer ingen melding om innsendt skjema til $topic.") }
+}
+
+class InnsendingProducer(
+    private val kafkaProducer: KafkaProducer<String, JsonElement>,
+) : InnsendingProducerI {
+    private val topic = getProperty("kafkaProducer.innsending.topic")
+
+    override fun send(vararg message: Pair<InnsendingKafka.Key, JsonElement>): Result<JsonElement> =
         message
             .toMap()
             .toJson()
             .let(::send)
+            .onSuccess {
+                sikkerLogger().info("Publiserte melding om innsendt skjema på topic $topic:\n${it.toPretty()}")
+            }.onFailure {
+                throw it.also {
+                    sikkerLogger().error(
+                        "Klarte ikke publisere melding om innsendt skjema på topic $topic:\n${
+                            message
+                                .toMap()
+                                .toJson().toPretty()
+                        }",
+                    )
+                }
+            }
 
     private fun Map<InnsendingKafka.Key, JsonElement>.toJson(): JsonElement =
         toJson(
