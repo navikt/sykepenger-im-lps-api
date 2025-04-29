742529b4c168dd394ec11d9cc7b0dd4ce88d7fa7
Magnus Sælensminde <magnus.salensminde@bekk.no>
2025-04-29 10:37:53 +0200

    Kjør lokalt med FakeUnleash

--- src/main/kotlin/no/nav/helsearbeidsgiver/utils/UnleashFeatureToggles.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/utils/UnleashFeatureToggles.kt
@@ -1,28 +1,32 @@
 package no.nav.helsearbeidsgiver.utils
 
 import io.getunleash.DefaultUnleash
+import io.getunleash.FakeUnleash
+import io.getunleash.Unleash
 import io.getunleash.util.UnleashConfig
 import no.nav.helsearbeidsgiver.Env
 
-class UnleashFeatureToggles {
-    private val apiKey = Env.getProperty("UNLEASH_SERVER_API_TOKEN")
-    private val apiUrl = Env.getProperty("UNLEASH_SERVER_API_URL") + "/api"
-    private val apiEnv = Env.getProperty("UNLEASH_SERVER_API_ENV")
-
-    private val defaultUnleash: DefaultUnleash =
-        DefaultUnleash(
-            UnleashConfig
-                .builder()
-                .appName("sykepenger-im-lps-api")
-                .instanceId("sykepenger-im-lps-api")
-                .unleashAPI(apiUrl)
-                .apiKey(apiKey)
-                .environment(apiEnv)
-                .build(),
-        )
+class UnleashFeatureToggles(
+    isLocalEnv: Boolean,
+) {
+    private val unleashClient: Unleash =
+        if (isLocalEnv) {
+            FakeUnleash()
+        } else {
+            DefaultUnleash(
+                UnleashConfig
+                    .builder()
+                    .appName("sykepenger-im-lps-api")
+                    .instanceId("sykepenger-im-lps-api")
+                    .unleashAPI(Env.getProperty("UNLEASH_SERVER_API_URL") + "/api")
+                    .apiKey(Env.getProperty("UNLEASH_SERVER_API_TOKEN"))
+                    .environment(Env.getProperty("UNLEASH_SERVER_API_ENV"))
+                    .build(),
+            )
+        }
 
     fun skalOppretteDialogVedMottattSykmelding(): Boolean =
-        defaultUnleash.isEnabled(
+        unleashClient.isEnabled(
             "opprett-dialog-ved-mottatt-sykmelding",
             false,
         )
