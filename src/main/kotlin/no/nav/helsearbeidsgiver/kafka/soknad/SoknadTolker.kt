3fd5d51606226d2110cf686090794e8e9777d60c
Magnus Sælensminde <magnus.salensminde@bekk.no>
2025-05-07 10:35:28 +0200

    Svelg exceptions for å komme up to date på sykepengesoknad topic i dev

--- src/main/kotlin/no/nav/helsearbeidsgiver/kafka/soknad/SoknadTolker.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/kafka/soknad/SoknadTolker.kt
@@ -1,6 +1,5 @@
 package no.nav.helsearbeidsgiver.kafka.soknad
 
-import kotlinx.serialization.SerializationException
 import no.nav.helsearbeidsgiver.kafka.MeldingTolker
 import no.nav.helsearbeidsgiver.utils.jsonMapper
 import no.nav.helsearbeidsgiver.utils.log.logger
@@ -13,13 +12,15 @@ class SoknadTolker : MeldingTolker {
     override fun lesMelding(melding: String) {
         try {
             val soknadMessage = jsonMapper.decodeFromString<SykepengesoknadDTO>(melding)
-            sikkerLogger.info("Mottok søknad med id ${soknadMessage.id} og sykmeldingId ${soknadMessage.sykmeldingId}.")
+            sikkerLogger.info(
+                "Mottok søknad med id ${soknadMessage.id}, sykmeldingId ${soknadMessage.sykmeldingId} og sendtNav ${soknadMessage.sendtNav}.",
+            )
             // TODO: Lagre søknad i database
         } catch (e: Exception) {
             val errorMsg = "Klarte ikke å lagre søknad om sykepenger!"
             logger.error(errorMsg)
             sikkerLogger.error(errorMsg, e)
-            throw SerializationException(errorMsg, e)
+            // throw SerializationException(errorMsg, e)
         }
     }
 }
