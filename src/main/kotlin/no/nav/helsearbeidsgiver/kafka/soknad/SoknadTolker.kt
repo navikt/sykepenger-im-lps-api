3985d049b1b0e31713953d0cb71c88565ee393a7
Gustav Berggren <83211434+b162214@users.noreply.github.com>
2025-05-07 12:55:27 +0200

    Lytt på søknader fra flex  (#96)
    Kopier søknadmodellen til Flex og deserialiser søknader fra sykepengesoknad-topicet.
    
    * Lag consumer som lytter på søknader
    
    * Fjern jackson
    Inline SykepengesoknadDTO med seializable
    
    * Fjern import av flex sin domenemodell, vanskelig å ta i mot med kotlinx
    
    * Oppdater testdata for soknad
    
    * Svelg exceptions for å komme up to date på sykepengesoknad topic i dev
    
    * Bruk enumverdier i søknadklasse og kast feil dersom serialisering tryner
    
    * Ta bort unødvendig justering av test
    
    * Feilhåndtering fiks
    
    ---------
    
    Co-authored-by: Magnus Sælensminde <magnus.salensminde@bekk.no>

---
+++ src/main/kotlin/no/nav/helsearbeidsgiver/kafka/soknad/SoknadTolker.kt
@@ -0,0 +1,26 @@
+package no.nav.helsearbeidsgiver.kafka.soknad
+
+import no.nav.helsearbeidsgiver.kafka.MeldingTolker
+import no.nav.helsearbeidsgiver.utils.json.fromJson
+import no.nav.helsearbeidsgiver.utils.log.logger
+import no.nav.helsearbeidsgiver.utils.log.sikkerLogger
+
+class SoknadTolker : MeldingTolker {
+    private val sikkerLogger = sikkerLogger()
+    private val logger = logger()
+
+    override fun lesMelding(melding: String) {
+        try {
+            val soknadMessage = melding.fromJson(SykepengesoknadDTO.serializer())
+            sikkerLogger.info(
+                "Mottok søknad med id ${soknadMessage.id}, sykmeldingId ${soknadMessage.sykmeldingId} og sendtNav ${soknadMessage.sendtNav}.",
+            )
+            // TODO: Lagre søknad i database
+        } catch (e: Exception) {
+            val errorMsg = "Klarte ikke å lagre søknad om sykepenger!"
+            logger.error(errorMsg)
+            sikkerLogger.error(errorMsg, e)
+            throw e
+        }
+    }
+}
