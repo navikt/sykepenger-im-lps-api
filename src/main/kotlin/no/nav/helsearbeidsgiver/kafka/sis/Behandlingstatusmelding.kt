2b9a199ea76dc75aadc2131b9d2a53f9d0c2cb2e
Magnus Sælensminde <magnus.salensminde@bekk.no>
2025-06-03 15:46:49 +0200

    Begynn å lese meldinger på tbd.sis

---
+++ src/main/kotlin/no/nav/helsearbeidsgiver/kafka/sis/Behandlingstatusmelding.kt
@@ -0,0 +1,31 @@
+@file:UseSerializers(OffsetDateTimeSerializer::class, UuidSerializer::class)
+
+package no.nav.helsearbeidsgiver.kafka.sis
+
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.UseSerializers
+import no.nav.helsearbeidsgiver.utils.json.serializer.OffsetDateTimeSerializer
+import no.nav.helsearbeidsgiver.utils.json.serializer.UuidSerializer
+import java.time.OffsetDateTime
+import java.util.UUID
+
+@Serializable
+data class Behandlingstatusmelding(
+    val vedtaksperiodeId: UUID,
+    val behandlingId: UUID,
+    val tidspunkt: OffsetDateTime,
+    val status: Behandlingstatustype,
+    val eksterneSøknadIder: Set<UUID>,
+) {
+    val versjon = "2.0.2" // TODO: Trengs denne?
+
+    @Serializable
+    enum class Behandlingstatustype {
+        OPPRETTET,
+        VENTER_PÅ_ARBEIDSGIVER,
+        VENTER_PÅ_ANNEN_PERIODE,
+        VENTER_PÅ_SAKSBEHANDLER,
+        FERDIG,
+        BEHANDLES_UTENFOR_SPEIL,
+    }
+}
---
+++ src/main/kotlin/no/nav/helsearbeidsgiver/kafka/sis/StatusISpeilTolker.kt
@@ -0,0 +1,30 @@
+package no.nav.helsearbeidsgiver.kafka.sis
+
+import no.nav.helsearbeidsgiver.kafka.MeldingTolker
+import no.nav.helsearbeidsgiver.soeknad.SoeknadRepository
+import no.nav.helsearbeidsgiver.utils.json.fromJson
+import no.nav.helsearbeidsgiver.utils.log.logger
+import no.nav.helsearbeidsgiver.utils.log.sikkerLogger
+
+class StatusISpeilTolker(
+    private val soeknadRepository: SoeknadRepository,
+) : MeldingTolker {
+    private val sikkerLogger = sikkerLogger()
+    private val logger = logger()
+
+    override fun lesMelding(melding: String) {
+        try {
+            val behandlingstatusmelding = melding.fromJson(Behandlingstatusmelding.serializer())
+            sikkerLogger.info(
+                "Mottok status-i-speil-melding med status ${behandlingstatusmelding.status}, " +
+                    "vedtaksperiodeId ${behandlingstatusmelding.vedtaksperiodeId} og eksterneSøknadIder " +
+                    "${behandlingstatusmelding.eksterneSøknadIder}.",
+            )
+        } catch (e: Exception) {
+            val errorMsg = "Klarte ikke å lagre status-i-speil-melding!"
+            logger.error(errorMsg)
+            sikkerLogger.error(errorMsg, e)
+            throw e
+        }
+    }
+}
