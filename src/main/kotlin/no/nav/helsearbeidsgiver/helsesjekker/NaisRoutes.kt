440f5fa0f27274e7ee208a955a06265f0dbc246d
Gustav Berggren <gustav.berggren@nav.no>
2025-06-18 13:50:29 +0200

    Debug readyness sjekk

--- src/main/kotlin/no/nav/helsearbeidsgiver/helsesjekker/NaisRoutes.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/helsesjekker/NaisRoutes.kt
@@ -5,6 +5,8 @@ import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
 import io.ktor.server.routing.route
+import no.nav.helsearbeidsgiver.utils.log.logger
+import no.nav.helsearbeidsgiver.utils.log.sikkerLogger
 import org.jetbrains.exposed.sql.Database
 
 fun Route.naisRoutes(database: Database) {
@@ -22,12 +24,17 @@ fun Route.isAlive() {
 
 fun Route.isReady(database: Database) {
     get("/is-ready") {
-        /*try {
+        try {
+            logger().debug("is-ready -- Start")
             val result = database.connector().prepareStatement("SELECT 1", true)
+            logger().debug("is-ready -- execute query")
             result.executeQuery()
+            logger().debug("is-ready -- End")
         } catch (e: Exception) {
+            logger().error("is-ready -- Exception: ${e.message}")
             call.respond(HttpStatusCode.InternalServerError, "Could not connect to database :(")
-        }*/
+        }
+        logger().info("is-ready -- Success")
         call.respond(HttpStatusCode.OK, "I am Ready :)")
     }
 }
