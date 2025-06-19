f7ea4826228cd98bcb35bfef4a4da7071fcb3be6
Gustav Berggren <gustav.berggren@nav.no>
2025-06-19 09:18:44 +0200

    Logg bedre error melding ved helsesjekk mot db også i åpne logger

--- src/main/kotlin/no/nav/helsearbeidsgiver/helsesjekker/HelseSjekkService.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/helsesjekker/HelseSjekkService.kt
@@ -1,5 +1,6 @@
 package no.nav.helsearbeidsgiver.helsesjekker
 
+import no.nav.helsearbeidsgiver.utils.log.logger
 import no.nav.helsearbeidsgiver.utils.log.sikkerLogger
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.transactions.transaction
@@ -13,7 +14,8 @@ class HelseSjekkService(
                 exec("SELECT 1") { rs -> rs.next() }
             } ?: false
         } catch (e: Exception) {
-            sikkerLogger().error(e.message, e)
+            logger().error("Helsesjekk mot database feilet")
+            sikkerLogger().error("Helsesjekk mot database feilet", e)
             false
         }
 }
