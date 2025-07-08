d1467ee42856c76e2dd7349183381f57d306116f
Morten Byhring <morten.byhring@nav.no>
2025-07-08 14:00:14 +0200

    opprydning: fjerner utdatert metode hentSykmelding(id, orgnr), benytt kun id

--- src/main/kotlin/no/nav/helsearbeidsgiver/sykmelding/SykmeldingService.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/sykmelding/SykmeldingService.kt
@@ -3,31 +3,11 @@ package no.nav.helsearbeidsgiver.sykmelding
 import no.nav.helsearbeidsgiver.sykmelding.model.Sykmelding
 import no.nav.helsearbeidsgiver.sykmelding.model.tilSykmelding
 import no.nav.helsearbeidsgiver.utils.log.logger
-import no.nav.helsearbeidsgiver.utils.log.sikkerLogger
 import java.util.UUID
 
 class SykmeldingService(
     private val sykmeldingRepository: SykmeldingRepository,
 ) {
-    fun hentSykmelding(
-        id: UUID,
-        orgnr: String,
-    ): Sykmelding? {
-        try {
-            val sykmeldingDTO = sykmeldingRepository.hentSykmelding(id).takeIf { it?.orgnr == orgnr }
-
-            if (sykmeldingDTO == null) {
-                logger().info("Fant ingen sykmeldinger $id for orgnr: $orgnr")
-                return null
-            }
-            sikkerLogger().info("Hentet sykmelding $id for orgnr: $orgnr")
-
-            return sykmeldingDTO.tilSykmelding()
-        } catch (e: Exception) {
-            throw RuntimeException("Feil ved henting av sykmelding $id orgnr: $orgnr", e)
-        }
-    }
-
     fun hentSykmelding(id: UUID): Sykmelding? = sykmeldingRepository.hentSykmelding(id)?.tilSykmelding()
 
     fun hentSykmeldinger(
