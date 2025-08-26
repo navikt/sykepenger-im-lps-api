420581428994ceff0f77335b9ec805e34de68b35
Magnus Sælensminde <magnus.salensminde@bekk.no>
2025-08-26 10:36:47 +0200

    Legg til innsendingfeil i responsen til LPS-apiet og lag forslag til feil vi kan returnere dersom validering i Simba mener IM er feil

--- src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/InntektsmeldingResponse.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/InntektsmeldingResponse.kt
@@ -10,6 +10,7 @@ import no.nav.helsearbeidsgiver.domene.inntektsmelding.v1.Inntekt
 import no.nav.helsearbeidsgiver.domene.inntektsmelding.v1.Inntektsmelding
 import no.nav.helsearbeidsgiver.domene.inntektsmelding.v1.Refusjon
 import no.nav.helsearbeidsgiver.domene.inntektsmelding.v1.skjema.SkjemaInntektsmelding
+import no.nav.helsearbeidsgiver.innsending.InnsendingFeil
 import no.nav.helsearbeidsgiver.innsending.InnsendingStatus
 import no.nav.helsearbeidsgiver.utils.json.serializer.LocalDateSerializer
 import no.nav.helsearbeidsgiver.utils.json.serializer.LocalDateTimeSerializer
@@ -34,11 +35,12 @@ data class InntektsmeldingResponse(
     val arbeidsgiver: Arbeidsgiver,
     val avsender: Avsender,
     val status: InnsendingStatus,
-    val statusMelding: String?,
+    val feil: InnsendingFeil?,
 )
 
 @Serializable
-data class InntektsmeldingRequest( // Innsending slik APIet sender inn
+data class InntektsmeldingRequest(
+    // Innsending slik APIet sender inn
     val navReferanseId: UUID,
     val agp: Arbeidsgiverperiode?,
     val inntekt: Inntekt?,
@@ -66,6 +68,7 @@ enum class InnsendingType {
                 is Inntektsmelding.Type.UtenArbeidsforhold,
                 is Inntektsmelding.Type.Behandlingsdager,
                 -> ARBEIDSGIVER_INITIERT
+
                 is Inntektsmelding.Type.ForespurtEkstern -> FORESPURT_EKSTERN
             }
     }
