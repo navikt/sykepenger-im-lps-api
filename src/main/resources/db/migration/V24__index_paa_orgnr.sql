871d245a10e16e8b4009ada32c15c4ea4e73753e
Morten Byhring <morten.byhring@nav.no>
2025-08-28 14:05:55 +0200

    PR kommentar: Fjern unødvendige indekser

--- src/main/resources/db/migration/V24__index_paa_orgnr.sql
+++ src/main/resources/db/migration/V24__index_paa_orgnr.sql
@@ -1,12 +1,9 @@
-CREATE INDEX ix_fsp_orgnr ON forespoersel (orgnr);
-CREATE INDEX ix_fsp_nav_referanse_id ON forespoersel (nav_referanse_id);
+CREATE INDEX IF NOT EXISTS ix_fsp_orgnr ON forespoersel (orgnr);
+CREATE INDEX IF NOT EXISTS ix_fsp_nav_referanse_id ON forespoersel (nav_referanse_id);
 
-CREATE INDEX ix_im_orgnr ON inntektsmelding (orgnr);
-CREATE INDEX ix_im_nav_referanse_id ON inntektsmelding (nav_referanse_id);
-CREATE INDEX ix_im_innsending_id_orgnr ON inntektsmelding (innsending_id, orgnr);
+CREATE INDEX IF NOT EXISTS ix_im_orgnr ON inntektsmelding (orgnr);
+CREATE INDEX IF NOT EXISTS ix_im_nav_referanse_id ON inntektsmelding (nav_referanse_id);
 
-CREATE INDEX ix_soknad_orgnr ON soknad (orgnr);
-CREATE INDEX ix_soknad_id ON soknad (soknad_id);
+CREATE INDEX IF NOT EXISTS ix_soknad_orgnr ON soknad (orgnr);
 
-CREATE INDEX ix_sm_orgnr ON sykmelding (orgnr);
-CREATE INDEX ix_sm_id ON sykmelding (sykmelding_id);
+CREATE INDEX IF NOT EXISTS ix_sm_orgnr ON sykmelding (orgnr);
