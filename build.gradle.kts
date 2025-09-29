5bb0f32971b933773d8bc24959ad53dc7283c7d6
Gustav Berggren <gustav.berggren@nav.no>
2025-09-29 13:29:59 +0200

    Fjern unødvendige env variabler

--- build.gradle.kts
+++ build.gradle.kts
@@ -115,7 +115,5 @@ tasks {
         environment("ALTINN_IM_RESSURS", "nav_system_sykepenger_inntektsmelding")
         environment("ALTINN_SM_RESSURS", "nav_system_sykepenger_sykmelding")
         environment("ALTINN_SOKNAD_RESSURS", "nav_system_sykepenger_soknad")
-        environment("PDL_URL", "http://dummy_url/pdl")
-        environment("PDL_SCOPE", "dummy_scope")
     }
 }
