11392cf654b8d80d4c8e472c59c1e812dd2b1cc9
Morten Byhring <morten.byhring@nav.no>
2025-11-19 08:15:27 +0100

    Revert "temp validering av naturalytelser-innsending fra API"
    This reverts commit 6216c44357d016d886681acd0249d57c2d5cb9bd.

--- src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/InntektsmeldingResponse.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/inntektsmelding/InntektsmeldingResponse.kt
@@ -59,9 +59,7 @@ data class InntektsmeldingRequest(
 ) {
     fun valider(): Set<String> {
         val naturalytelserFeilmelding =
-            if (naturalytelser != inntekt?.naturalytelser) { // temp validering
-                "naturalytelser kan ikke være forskjellig fra inntekt.naturalytelser"
-            } else if (naturalytelser.all { it.verdiBeloep > 0 && it.verdiBeloep < 1_000_000 }) {
+            if (naturalytelser.all { it.verdiBeloep > 0 && it.verdiBeloep < 1_000_000 }) {
                 null
             } else {
                 "Beløp må være større enn 0"
