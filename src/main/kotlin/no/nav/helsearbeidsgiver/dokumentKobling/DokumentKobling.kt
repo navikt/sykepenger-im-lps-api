1bf4492accfbce8a950cc85eaf126e26a071949b
Gustav Berggren <gustav.berggren@nav.no>
2025-11-24 11:13:26 +0100

    Oppdater dokumentkobling melding struktur
    Co-authored-by: Magnus Sælensminde <magnus.selensminde@nav.no>
    Co-authored-by: Jesper Hustad <Jesper.Forrest.Hustad@nav.no>

--- src/main/kotlin/no/nav/helsearbeidsgiver/dokumentKobling/DokumentKobling.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/dokumentKobling/DokumentKobling.kt
@@ -6,7 +6,6 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.UseSerializers
 import no.nav.helsearbeidsgiver.domene.inntektsmelding.v1.Periode
-import no.nav.helsearbeidsgiver.innsending.InnsendingStatus
 import no.nav.helsearbeidsgiver.utils.json.serializer.LocalDateSerializer
 import no.nav.helsearbeidsgiver.utils.json.serializer.UuidSerializer
 import no.nav.helsearbeidsgiver.utils.wrapper.Orgnr
@@ -35,75 +34,52 @@ data class Sykepengesoeknad(
 ) : DokumentKobling()
 
 @Serializable
-@SerialName("VedtaksperiodeKobling")
-data class VedtaksperiodeKobling(
+@SerialName("VedtaksperiodeSoeknadKobling")
+data class VedtaksperiodeSoeknadKobling(
     val vedtaksperiodeId: UUID,
     val soeknadId: UUID,
 ) : DokumentKobling()
 
-// Skal vi bruke disse to:
 @Serializable
 @SerialName("ForespoerselSendt")
 data class ForespoerselSendt(
-    val forespoerselId: UUID,
-    val vedtaksperiodeId: UUID,
-    val orgnr: Orgnr,
+    val forespoerselKobling: ForespoerselKobling,
 ) : DokumentKobling()
 
 @Serializable
 @SerialName("ForespoerselUtgaatt")
 data class ForespoerselUtgaatt(
-    val forespoerselId: UUID,
-    val vedtaksperiodeId: UUID,
-    val orgnr: String,
+    val forespoerselKobling: ForespoerselKobling,
 ) : DokumentKobling()
 
-// ...eller denne?
-@Serializable
-@SerialName("Forespoersel")
-data class Forespoersel(
-    val forespoerselId: UUID,
-    val vedtaksperiodeId: UUID,
-    val orgnr: String,
-    val status: ForespoerselStatus,
-) : DokumentKobling()
-
-enum class ForespoerselStatus {
-    UTGAATT,
-    AKTIV,
-}
-
-// Og skal vi bruke denne?
-@Serializable
-@SerialName("Inntektsmelding")
-data class Inntektsmelding(
-    val forespoerselId: UUID,
-    val innsendingId: UUID,
-    val orgnr: String,
-    val status: InnsendingStatus,
-) : DokumentKobling()
-
-// Eller flate det ut til disse tre?
 @Serializable
 @SerialName("InntektsmeldingMottatt")
 data class InntektsmeldingMottatt(
-    val forespoerselId: UUID,
-    val innsendingId: UUID,
-    val orgnr: String,
+    val inntektsmeldingKobling: InntektsmeldingKobling,
 ) : DokumentKobling()
 
 @Serializable
 @SerialName("InntektsmeldingAvvist")
 data class InntektsmeldingAvvist(
-    val forespoerselId: UUID,
-    val innsendingId: UUID,
-    val orgnr: String,
+    val inntektsmeldingKobling: InntektsmeldingKobling,
 ) : DokumentKobling()
 
 @Serializable
 @SerialName("InntektsmeldingGodkjent")
 data class InntektsmeldingGodkjent(
-    val forespoerselId: UUID,
-    val innsendingId: UUID,
-    val orgnr: String,
+    val inntektsmeldingKobling: InntektsmeldingKobling,
 ) : DokumentKobling()
+
+@Serializable
+abstract class ForespoerselKobling(
+    val forespoerselId: UUID,
+    val vedtaksperiodeId: UUID,
+    val orgnr: String,
+)
+
+@Serializable
+abstract class InntektsmeldingKobling(
+    val innsendingId: UUID,
+    val forespoerselId: UUID,
+    val orgnr: String,
+)
