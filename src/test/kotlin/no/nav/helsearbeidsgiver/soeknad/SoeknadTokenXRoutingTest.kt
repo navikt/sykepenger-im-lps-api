e898d987b15bcd8ea993732e6450200214555b92
Jesper Hustad <jesper.forrest.hustad@nav.no>
2026-04-15 16:59:33 +0200

    Faktisk fiks tester

--- src/test/kotlin/no/nav/helsearbeidsgiver/soeknad/SoeknadTokenXRoutingTest.kt
+++ src/test/kotlin/no/nav/helsearbeidsgiver/soeknad/SoeknadTokenXRoutingTest.kt
@@ -96,7 +96,7 @@ class SoeknadTokenXRoutingTest : ApiTest() {
             SykepengeSoeknadDto(Random.nextLong(), soeknadMock().medId(soeknadId).medOrgnr(DEFAULT_ORG))
         runBlocking {
             val response =
-                client.get("/intern/personbruker/sykepengesoeknad/$soeknadId/pdf") {
+                client.get("/intern/personbruker/soknad/$soeknadId/pdf") {
                     bearerAuth(mockOAuth2Server.gyldigTokenxToken(DEFAULT_FNR))
                 }
             response.status shouldBe HttpStatusCode.Unauthorized
