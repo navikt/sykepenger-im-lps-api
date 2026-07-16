38b16991d0ce2c1e3769f863eb7b3868b2a2b314
Mehdi.Zare <mehdi.zare@nav.no>
2026-07-16 11:22:47 +0200

    inntekt endepunkt

--- src/main/kotlin/no/nav/helsearbeidsgiver/inntekt/Inntekt.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/inntekt/Inntekt.kt
@@ -4,10 +4,12 @@
 
 package no.nav.helsearbeidsgiver.inntekt
 
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.UseSerializers
 import no.nav.helsearbeidsgiver.utils.json.serializer.YearMonthSerializer
 import java.time.YearMonth
 
+@Serializable
 data class Inntekt(
     val inntekt: Map<YearMonth, Double?>,
     val gjennomsnittInntekt: Double =
