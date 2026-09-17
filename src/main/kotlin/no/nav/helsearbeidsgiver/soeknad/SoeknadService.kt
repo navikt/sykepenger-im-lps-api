092d1b586acce18d8c5e382f2b471ccced4f8e86
mortenbyhring <117746154+mortenbyhring@users.noreply.github.com>
2026-09-17 15:08:03 +0200

    Alle nye søknader som lagres skal også vises til arbeidsgiver (#349)
    * Alle nye søknader som lagres skal også deles og vises til arbeidsgiver

--- src/main/kotlin/no/nav/helsearbeidsgiver/soeknad/SoeknadService.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/soeknad/SoeknadService.kt
@@ -10,7 +10,11 @@ import no.nav.helsearbeidsgiver.utils.log.logger
 import no.nav.helsearbeidsgiver.utils.log.sikkerLogger
 import no.nav.helsearbeidsgiver.utils.whitelistetForArbeidsgiver
 import no.nav.helsearbeidsgiver.utils.wrapper.Orgnr
+import java.time.LocalDateTime
 import java.util.UUID
+
+// Kan fjernes dersom vi sletter gamle søknader helt opp til dette tidspunktet
+private val START_TID_FOR_VISNING_ALLE_SOEKNADER = LocalDateTime.of(2026, 9, 18, 12, 0)
 
 class SoeknadService(
     val soeknadRepository: SoeknadRepository,
@@ -122,7 +126,13 @@ class SoeknadService(
             !erEttersendtTilNAV() &&
             this.status == SykepengeSoeknadKafkaMelding.SoknadsstatusDTO.SENDT
 
-    private fun SykepengeSoeknadKafkaMelding.skalSendesTilArbeidsgiver(): Boolean = this.sendtArbeidsgiver != null
+    // ALLE nye søknader skal sendes og vises, men vi kan ikke plutselig begynne å returnere gamle (de er ikke distribuert)
+    private fun SykepengeSoeknadKafkaMelding.skalSendesTilArbeidsgiver(): Boolean =
+        (
+            this.sendtNav != null &&
+                this.sendtNav > START_TID_FOR_VISNING_ALLE_SOEKNADER
+        ) ||
+            this.sendtArbeidsgiver != null
 
     private fun SykepengeSoeknadKafkaMelding.validerPaakrevdeFelter(): LagreSoeknad =
         LagreSoeknad(
